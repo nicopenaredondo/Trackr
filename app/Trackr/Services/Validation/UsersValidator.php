fd066c2be58fc3390ab30bcd6802a9e9472fc8f3
Nico Penaredondo <npenaredondo@gmail.com>
2014-07-28 22:09:16 +0800

    modified the UsersValidator

--- app/Trackr/Services/Validation/UsersValidator.php
+++ app/Trackr/Services/Validation/UsersValidator.php
@@ -5,11 +5,23 @@ class UsersValidator extends Validator
 
 	static $insertRules = [
 		'username' => 'required|max:32',
-		'password' => 'required|max:64'
+		'password' => 'required|max:64',
+		//'department_id' => 'required|integer',
+		'group_id' 			=> 'required|integer',
+		'job_id' 				=> 'required|integer',
+		'first_name' 		=> 'required|max:64',
+		'last_name' 		=> 'required|max:64',
+		'birthday' 			=> 'required|date',
+		'email' 				=> 'required|email',
+		'address' 			=> 'required|max:1000',
+		'phone_number'  => 'required|max:32',
+		'emergency_contact_number' => 'required|max:32',
+		'emergency_contact_name'   => 'required|max:64'
 	];
 
 	static $updateRules = [
 		//'department_id' => 'required|integer',
+		'group_id' 			=> 'required|integer',
 		'job_id' 				=> 'required|integer',
 		'first_name' 		=> 'required|max:64',
 		'last_name' 		=> 'required|max:64',
