617dd146e5017aee3285cfc9b25ca7b46d40511a
Nico Penaredondo <npenaredondo@gmail.com>
2014-07-28 22:09:26 +0800

    added the AttendancesValidator

---
+++ app/Trackr/Services/Validation/AttendancesValidator.php
@@ -0,0 +1,17 @@
+<?php namespace Trackr\Services\Validation;
+
+class AttendancesValidator extends Validator
+{
+
+	static $insertRules = [
+		'user_id' => 'required|integer',
+		'time_in' => 'required|date',
+	];
+
+	static $updateRules = [
+		'user_id'   => 'integer',
+		'time_in' 	=> 'required|date',
+		'time_out'	=> 'required|date'
+	];
+
+}
