b1cd6caa28ee1426378e07ad4fc555f39b59172b
Nico Penaredondo <npenaredondo@gmail.com>
2014-07-28 22:11:42 +0800

    adde login feature of users

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -5,17 +5,29 @@ class AuthController extends BaseController
 
 	public function getLogin()
 	{
-
+		return View::make('auth.login');
 	}
 
 	public function postLogin()
 	{
+		$credentials = [
+		'username' => Input::get('username'),
+		'password' => Input::get('password')
+		];
 
+		if(Auth::attempt($credentials)){
+			return Redirect::route('app.dashboard');
+		}else{
+			return Redirect::route('app.auth.login')
+			               ->with('error','Invalid username or password');
+		}
 	}
 
 	public function getLogout()
 	{
-
+		Auth::logout();
+		return Redirect::route('app.auth.login')
+									->with('success', 'You are now logged out.');
 	}
 
 	public function getForgotPassword()
