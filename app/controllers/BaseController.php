f59c25093ff4a92c9c2df81591a2c2241ad27932
Nico Penaredondo <npenaredondo@gmail.com>
2014-07-31 20:56:20 +0800

    added dashboards in BaseController

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -1,6 +1,55 @@
 <?php
 
-class BaseController extends Controller {
+//repository
+use Trackr\Repository\Announcements\InterfaceAnnouncementsRepository as AnnouncementRepository;
+use Trackr\Repository\Projects\InterfaceProjectsRepository as ProjectRepository;
+use Trackr\Repository\Departments\InterfaceDepartmentsRepository as DepartmentRepository;
+use Trackr\Repository\Users\InterfaceUsersRepository as UserRepository;
+
+class BaseController extends Controller
+{
+
+	/**
+	 * Announcement Repository
+	 *
+	 * @param  \Trackr\Repository\Announcements\InterfaceAnnouncementsRepository
+	 */
+	protected $announcement;
+
+	/**
+	 * Project Repository
+	 *
+	 * @param  \Trackr\Repository\Projects\InterfaceProjectsRepository
+	 */
+	protected $project;
+
+	/**
+	 * Department Repository
+	 *
+	 * @param  \Trackr\Repository\Departments\InterfaceDepartmentsRepository
+	 */
+	protected $department;
+
+	/**
+	 * User Repository
+	 *
+	 * @param  \Trackr\Repository\Users\InterfaceUsersRepository
+	 */
+	protected $user;
+
+
+	public function __construct(
+		AnnouncementRepository $announcement,
+		ProjectRepository $project,
+		DepartmentRepository $department,
+		UserRepository $user
+	)
+	{
+		$this->announcement = $announcement;
+		$this->project  		= $project;
+		$this->department  	= $department;
+		$this->user 				= $user;
+	}
 
 	/**
 	 * Setup the layout used by the controller.
@@ -17,7 +66,48 @@ class BaseController extends Controller {
 
 	public function getDashboard()
 	{
-		return View::make('backend.dashboard.index');
+
+		if(Auth::user()->group_id == 1) {
+			$countOfAnnouncement 	= $this->announcement->count();
+			$countOfProject 			= $this->project->count();
+			$countOfDepartment 		= $this->department->count();
+			$countOfUser 					= $this->user->count();
+
+			$listOfAnnouncement 	= $this->announcement->getRecentRecord(5);
+			$listOfProject 				= $this->project->getRecentRecord(5);
+			$listOfDepartment 		= $this->department->getRecentRecord(5);
+			$listOfUser 					= $this->user->getRecentRecord(5);
+
+			$data = [
+				'countOfAnnouncement', 'countOfProject', 'countOfDepartment', 'countOfUser',
+				'listOfAnnouncement', 'listOfProject', 'listOfDepartment', 'listOfUser'
+			];
+			return View::make('backend.dashboard.index-admin',compact($data));
+		}else{
+
+			$listOfAnnouncement 	= $this->announcement->getRecentRecord(5);
+			$data = ['listOfAnnouncement'];
+			return View::make('backend.dashboard.index-user',compact($data));
+
+		}
+
+
+
+	}
+
+	public function getChangePassword()
+	{
+		return View::make('backend.change-password');
+	}
+
+	public function postChangePassword()
+	{
+		$user = User::find(Auth::user()->user_id);
+		$user->fill(Input::all());
+		if($user->save()){
+			return Redirect::route('app.change-password')
+										->with('success', 'You have successfully change your password');
+		}
 	}
 
 }
