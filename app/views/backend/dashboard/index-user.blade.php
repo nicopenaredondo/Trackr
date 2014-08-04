3a8c140979d50d42f8074e6fab6ee5ba92914083
Nico Penaredondo <npenaredondo@gmail.com>
2014-08-04 11:12:47 +0800

    implemented the full calendar plugin

--- app/views/backend/dashboard/index-user.blade.php
+++ app/views/backend/dashboard/index-user.blade.php
@@ -2,6 +2,11 @@
 @section('site_title')
 Dashboard
 @stop
+
+@section('header_styles')
+{{ HTML::style('assets/plugins/full-calendar/fullcalendar.css') }}
+@stop
+
 @section('content')
 	<!-- Begin page heading -->
 	<h1 class="page-heading">Dashboard</h1>
@@ -11,42 +16,102 @@ Dashboard
 			<div class="col-md-12 col-xs-12">
 				<div class="panel panel-success panel-square panel-no-border">
 				  <div class="panel-heading">
-						<h3 class="panel-title">Announcements</h3>
-						<div class="right-content">
-							<a href="{{ URL::route('announcements.index') }}" class="btn btn-xs btn-success">
-								<i class="fa fa-list"></i> View All Announcements
-							</a>
-						</div>
+						<h3 class="panel-title"><i class="fa fa-calendar"></i> Calendar</h3>
+
 				  </div>
 				  <div class="panel-body">
-				  @if(count($listOfAnnouncement) > 0)
-					<table class="table table-striped">
-						<thead>
-							<tr>
-								<th>Title</th>
-								<th>Posted At</th>
-								<th>Action</th>
-							</tr>
-						</thead>
-						<tbody>
-							@foreach($listOfAnnouncement as $announcement)
-							<tr>
-								<td>{{ Str::limit($announcement['announcement_title'],30) }}</td>
-								<td>{{ date('F j, Y', strtotime($announcement['created_at']))  }}</td>
-								<th>
-									<a href="{{ URL::route('announcements.show', $announcement['announcement_id']) }}" class="btn btn-default btn-xs"><i class="fa fa-eye"></i> View Announcement</a>
-								</th>
-							</tr>
-							@endforeach
-						</tbody>
-					</table>
-					@else
-						<div class="alert alert-info square fade in alert-dismissable" style="margin-top:10px;">
-						  <strong>Information!</strong> You don't have any announcements in the system.
+						<div class="row">
+							<div class="col-md-4">
+								<div class="panel panel-danger panel-square panel-no-border">
+								  <div class="panel-heading">
+										<h3 class="panel-title"><i class="fa fa-tasks"></i> Today's task</h3>
+									</div>
+								  <div class="panel-body">
+										@if(count($listOfTodayTasks) > 0)
+											@foreach($listOfTodayTasks as $task)
+												<div class="row task">
+													<div class="col-md-12">
+														<div class="the-box">
+															<div class="media user-card-sm">
+															  <div class="row">
+															  	<div class="col-md-12">
+															  		<div class="media-body text-center">
+																			<h4 class="media-heading">{{ $task['task_name'] }}</h4>
+																			<p class="text-info">{{ $task['task_due_date_human'] }}</p>
+																	  </div>
+															  	</div>
+															  </div>
+															  <div class="row">
+															  	<div class="col-md-12 text-center">
+															  		<div class="btn-group">
+																			<a href="" data-task-id="{{$task['task_id']}}" class="btn btn-primary task-done"><i class="fa fa-check"></i></a>
+																			<a href="{{ URL::route('tasks.edit', $task['task_id']) }}" class="btn btn-warning"><i class="fa fa-pencil"></i></a>
+																			<a href="" data-task-id="{{$task['task_id']}}" class="btn btn-danger task-remove"><i class="fa fa-times"></i></a>
+																		</div>
+															  	</div>
+															  </div>
+															</div>
+														</div>
+													</div>
+												</div>
+											@endforeach
+										@else
+											<div class="alert alert-info square fade in alert-dismissable" style="margin-top:10px;">
+											  <strong>Hooray!</strong> You don't have any task for today
+											</div>
+										@endif
+									</div><!-- /.panel-body -->
+								</div>
+							</div>
+							<div class="col-md-8">
+								<div id="calendar"></div>
+							</div>
 						</div>
-					@endif
 					</div><!-- /.panel-body -->
 				</div>
 			</div>
 		</div>
+@stop
+
+@section('footer_script')
+{{ HTML::script('assets/plugins/full-calendar/lib/moment.min.js') }}
+{{ HTML::script('assets/plugins/full-calendar/fullcalendar.js') }}
+<script>
+	$(document).ready(function() {
+
+	    // page is now ready, initialize the calendar...
+
+	    $('#calendar').fullCalendar({
+	        // put your options and callbacks here
+	        events: {{ $listOfTasks }}
+	    });
+
+	    $('.task-remove').click(function(){
+				var task   = $(this);
+				$.post('{{ URL::route("tasks.remove") }}',{ 'task_id' : task.attr('data-task-id') },function(){
+					task.closest('.task').hide('slow',function(){
+       			task.closest('.task').remove();
+       		});
+				});
+
+				return false;
+			});
+
+			$('.task-done').click(function(){
+				var task   = $(this);
+				var data   = {
+					'task_id' : task.attr('data-task-id')
+				};
+				$.post('{{ URL::route("tasks.change-status") }}', data,function(){
+					task.closest('.task').hide('slow',function(){
+       			task.closest('.task').remove();
+       		});
+				});
+
+				return false;
+			});
+
+	});
+
+</script>
 @stop
