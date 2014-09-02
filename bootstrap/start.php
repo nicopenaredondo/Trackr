0e670742af65c70ea73680de82f7c58249603fb1
Nico Penaredondo <npenaredondo@gmail.com>
2014-09-02 15:03:55 +0800

    added my workstation(office) in the local environment

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('HSLNicoP-PC','your-hostname-here'),
+	'local' => array('HSLNicoP-PC','SAMNLPC0017'),
 
 ));
 
